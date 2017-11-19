fa988ce180ceeb5248ba2854646c5a7581b537ed
hroad <1043431651@qq.com>
2017-11-19 22:34:16 +0800

    update

--- src/js/app.js
+++ src/js/app.js
@@ -109,4 +109,66 @@ $(function(){
 //		客服               END		
 		
 		
+	
+		
 });
+
+
+$(function(){
+//	无缝滚动
+	var oul = $('.text_pic ul');
+	var oulHtml = oul.html();
+	oul.html(oulHtml+oulHtml)
+	var timeId = null;
+
+	var ali = $('.text_pic ul li');
+	var aliWidth = ali.eq(0).width();
+	var aliSize = ali.size();
+	var ulWidth = aliWidth*aliSize;
+	oul.width(ulWidth);	//1600px
+	
+	var speed = 2;
+
+	function slider(){
+
+		if(speed<0){
+			if(oul.css('left')==-ulWidth/2+'px'){
+	 		oul.css('left',0);
+		 	}
+		 	oul.css('left','+=-2px');
+		}
+
+	 	
+		if(speed>0){
+			if(oul.css('left')=='0px'){
+	 		oul.css('left',-ulWidth/2+'px');
+		 	}
+		 	oul.css('left','+='+speed+'px');
+		}
+	 	
+	 }
+	
+	// setInterval()函数的作用是：每隔一段时间，执行该函数里的代码
+	 timeId = setInterval(slider,30);
+
+	$('.text_pic').mouseover(function(){
+		// clearInterval()函数的作用是用来清除定时器
+		clearInterval(timeId);
+	});
+
+	$('.text_pic').mouseout(function(){
+		timeId = setInterval(slider,30);
+	});
+
+//	$('.goLeft').click(function(){
+//		speed=-2;
+//	});
+//
+//	$('.goRight').click(function(){
+//		speed=2;
+//	});
+});
+//	无缝滚动      END
+
+
+
